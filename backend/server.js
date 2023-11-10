fbfd47401b8928b829ade44abc4f73b03ebf37f4
Frankebuka <frankebuka@yahoo.com>
2023-11-10 11:35:07 +0000

    Add and send data from backend server and install nodemon and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import { chats } from "./data/data.js";
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const chat = chats.find((c) => c._id === req.params.id);
+  res.send(chat);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
