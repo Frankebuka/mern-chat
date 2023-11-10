24b073c070fb27dfe06a27c018bbc148330f4f8f
Frankebuka <frankebuka@yahoo.com>
2023-11-10 13:53:12 +0000

    testing api on our frontend and created our models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+// import mongoose from "mongoose";
 import { chats } from "./data/data.js";
 
 dotenv.config();
+
+// mongoose.connect();
 const app = express();
+
+app.use(express.json());
 
 app.get("/api/chat", (req, res) => {
   res.send(chats);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    // const res = await fetch("/api/chat");
+    // const data = await res.json();
+    console.log(data);
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>{chats && chats.map((d) => <div key={d._id}>{d.chatName}</div>)}</div>
+  );
 };
 
 export default Home;
